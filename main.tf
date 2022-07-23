00edb94c57e3db6cbcde6fb4f0ff3c111f37e3f3
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-07-23 15:13:20 +0700

    temp: remove more providers

--- main.tf
+++ main.tf
@@ -9,18 +9,18 @@ terraform {
       source  = "hashicorp/azuread"
       version = "2.26.1"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.12.1"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.6.0"
-    }
+    # kubernetes = {
+    #   source  = "hashicorp/kubernetes"
+    #   version = "2.12.1"
+    # }
+    # kubectl = {
+    #   source  = "gavinbunney/kubectl"
+    #   version = ">= 1.14.0"
+    # }
+    # helm = {
+    #   source  = "hashicorp/helm"
+    #   version = "2.6.0"
+    # }
   }
 }
 # provider "kubernetes" {
